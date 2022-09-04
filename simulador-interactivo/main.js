a5ea04b94a3748a999dfbfcaa2e119baa222560f
rooramirez <roomeli.ramirez@gmail.com>
2022-09-04 20:09:30 -0300

    Se agrega pirmera parte del simulador interactivo

--- simulador-interactivo/main.js
+++ simulador-interactivo/main.js
@@ -14,3 +14,72 @@
 // let promedioNotas = calcularPromedio(sumaNotas(nota1, nota2, nota3, nota4), 4);
 
 // console.log(promedioNotas);
+
+
+//-----------------------------------------------proyecto final------------------------------------------------------
+
+
+//Aplicacion Web: "Armonía familiar" - Lista de tareas: 
+
+//Variables: 
+const arrayTareas = [];
+
+// const tarea ={
+//     nombre: "",
+//     descripcion: "",
+//     seccion: "",
+//     recurrencia: "",
+//     responsable: ""
+// }
+class Tarea{
+    constructor(nombre, descripcion, seccion, recurrencia, responsable) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.seccion = seccion;
+        this.recurrencia = recurrencia;
+        this.responsable = responsable;
+    }
+}
+
+
+const agregarTarea = () => {
+   //variables para agregar una nueva tarea:
+    let nombreTarea = prompt('Ingrese nombre de la tarea:');
+    let descripcionTarea = prompt('Ingrese descripcion de la tarea:');
+    let seccionTarea = prompt('Ingrese nombre de la tarea:');
+    let recurrenciaTarea = prompt('Ingrese la frecuencia de la tarea:');
+    let responsableTarea = prompt('Ingrese el nombre del responsable de la tarea:');
+    arrayTareas.push(new Tarea(nombreTarea, descripcionTarea, seccionTarea, recurrenciaTarea, responsableTarea));
+
+}
+
+const verListaTarea = () => {
+
+}
+
+const verStockAlimentos = () => {
+
+}
+
+
+let ingresoALaAplicacion = prompt('Bienvenido a la aplicacion web "Armonía Familiar", destinado a la organizacion y division de tareas equitativamente. ¿Que desea hacer? Ingrese: 1 para agregar una tarea, 2 para ver lista de tareas ó 3 para ver el stock dispoble de alimentos');
+
+    switch (ingresoALaAplicacion) {
+        case '1':
+            agregarTarea();
+            alert('Se agrego la nueva tarea con exito!' `${arrayTareas[arrayTareas.length-1]}` );
+            break;
+        case '2':
+            verListaTarea();
+            break;
+        case '3':
+            verStockAlimentos();
+            break;
+
+        default:
+            break;
+    }
+
+
+
+
